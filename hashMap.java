4df9b88f2296c4fff3b7ff797a9da5dc8d9004ae
Davi Monteiro Cardoso <davilopeszin@gmail.com>
2026-01-08 20:10:05 -0300

    hashmaps

---
+++ hashMap.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class hashMap {
+    public static void main(String[] args) {
+        HashMap<String, Double> items = new HashMap<>();
+
+        items.put("pizza", 9.99);
+        items.put("hamburguer", 6.99);
+        items.put("hotdog", 4.99);
+        items.put("taco", 5.99);
+        items.put("sushi", 14.99);
+
+
+        // Print keys & values
+        for (String value : items.keySet()) {
+            System.out.println(value + ": $" + items.get(value));
+        }
+
+    }
+}
