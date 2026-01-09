bb8693902558b52ee0f149b449a358da7e560c1f
Davi Monteiro Cardoso <davilopeszin@gmail.com>
2026-01-09 02:41:34 -0300

    comments
    Added input handling to read a word and extract vowels.

--- vowels.java
+++ vowels.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class vowels {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
         String word = input.nextLine();
 
         System.out.println(vogalRun(word));
     }
+    // Extract vowels from word
     static String vogalRun(String word) {
         String vogaisWord = "";
         char[] vogais = {'a', 'e', 'i', 'o', 'u'};
