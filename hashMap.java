dbe4953245836eea778468589d0ef9e50e313972
Davi Monteiro Cardoso <davilopeszin@gmail.com>
2026-01-09 00:55:53 -0300

    Comments

--- hashMap.java
+++ hashMap.java
@@ -2,8 +2,10 @@ import java.util.HashMap;
 
 public class hashMap {
     public static void main(String[] args) {
+        
         HashMap<String, Double> items = new HashMap<>();
 
+        // Keys & values
         items.put("pizza", 9.99);
         items.put("hamburguer", 6.99);
         items.put("hotdog", 4.99);
